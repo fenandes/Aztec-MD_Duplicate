eb91f6af33c00db7cf1ac3fd0619b66178687e77
D3centX <109889758+DiegosonTech@users.noreply.github.com>
2023-10-20 17:54:48 +0200

    vorterx

--- mangoes/mongo_connect/mongo_connect.js
+++ mangoes/mongo_connect/mongo_connect.js
@@ -1,4 +1,6 @@
-const { sessionSchema } = require("./connicto");
+const { sessionSchema } = require("./connicto/SessionSchema.js");
+const { sessionSchema } = require("./connicto/core.js");
+
 module.exports = class Database {
   constructor() {}
   getSession = async (sessionId) => await this.session.findOne({ sessionId });
