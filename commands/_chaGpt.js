e25b1c735e9cdb566e417e42263e8a52cfd26778
D3centX <109889758+DiegosonTech@users.noreply.github.com>
2023-10-20 06:13:42 +0200

    vorterx

--- commands/_chaGpt.js
+++ commands/_chaGpt.js
@@ -7,13 +7,13 @@ module.exports = {
   category: "CHATGPT",
   desc: "Research something",
   async xstart(vorterx, m, { xReact, doReply, text }) {
+     
     if (!text) {
-      await xReact("❌");
-      return m.reply("*Please provide a query, e.g., 'Who is Aztec?'*");
+    await xReact("❌");
+    return m.reply("*Please provide a query, e.g., 'Who is Aztec?'*");
     }
 
     await xReact("🤖");
-
     const response = `
 *CHAT_GPT - GPT-4 2023*
 
@@ -21,11 +21,11 @@ Query: ${text}
 
 [![Thumbnail](https://i.ibb.co/C7TXRcH/photo-1678483789107-0029c61fdcca.jpg)]
 
-GPT-4 is the latest version of the Chat_GPT language model, providing even more advanced conversational capabilities and natural language understanding.
+AZTEC MD is the latest version of the Chat_GPT4 language model, providing even more advanced conversational capabilities and natural language understanding.
 
-Feel free to ask any question, and I'll do my best to provide a helpful response!
+Feel free to ask any question, and I'll do my best to provide a helpful answer!
 `;
 
-    await doReply(vorterx, m, response);
-  },
+await m.reply(vorterx, m, response);
+},
 };
