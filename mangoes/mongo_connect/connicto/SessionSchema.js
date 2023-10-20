c3a32a7ed5aa7e12bc9c46e5dcd6c0d0fae50fc4
D3centX <109889758+DiegosonTech@users.noreply.github.com>
2023-10-20 17:19:28 +0200

    vorterx

---
+++ mangoes/mongo_connect/connicto/SessionSchema.js
@@ -0,0 +1,10 @@
+const { model, Schema } = require("mongoose");
+const schema = new Schema({
+sessionId: {
+type: String,
+required: true,
+unique: true,
+ },
+session: String,
+});
+module.exports = model("sessionschemas", schema);
