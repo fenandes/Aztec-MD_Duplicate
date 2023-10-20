b65bd3c57710ba6894880bfdfe1b913fd72d3fd9
D3centX <109889758+DiegosonTech@users.noreply.github.com>
2023-10-20 17:01:22 +0200

    vorterx

--- core.js
+++ core.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const { default: WAConnection,DisconnectReason,Browsers,fetchLatestBaileysVersion,makeInMemoryStore,delay,useMultiFileAuthState} = require('@adiwajshing/baileys');
+const {default: WAConnection,DisconnectReason,Browsers,fetchLatestBaileysVersion,makeInMemoryStore,delay,useMultiFileAuthState} = require('@adiwajshing/baileys');
 const { Boom } = require('@hapi/boom');
 const P = require('pino');
 const { QuickDB } = require('quick.db');
 const moment = require('moment-timezone');
 const fs = require('fs');
+const qrcode = require('qrcode');
 const config = require('./config');
 const { Collection } = require('discord.js');
 const contact = require('./mangoes/contact.js');
@@ -18,12 +19,11 @@ let qr_gen = "invalid";
 async function startAztec() {
   const { version } = await fetchLatestBaileysVersion();
   const { state, saveState, clearState } = await getAuthFromDatabase();
-const { getAuthFromDatabase } = new Auth(sessionId);
 
-  const vorterx = WAConnection
+  const vorterx = WAConnection();
   vorterx.logger = P({ level: 'silent' });
   vorterx.printQRInTerminal = false;
-  vorterx.browser = Browser.macOS("Desktop");
+  vorterx.browser = Browsers.macOS("Desktop");
   vorterx.qrTimeout = undefined;
   vorterx.auth = state;
   vorterx.version = version;
@@ -37,26 +37,25 @@ const { getAuthFromDatabase } = new Auth(sessionId);
   await readCommands(vorterx);
 
   vorterx.ev.on('creds.update', saveCreds);
-
   vorterx.ev.on('connection.update', async (update) => {
   const { connection, lastDisconnect } = update;
 
     if (
-   connection === 'close' ||
-   connection === 'lost' ||
-   connection === 'restart' ||
-   connection === 'timeout'
-    ) {
-    let reason = new Boom(lastDisconnect?.error)?.output.statusCode;
+      connection === 'close' ||
+      connection === 'lost' ||
+      connection === 'restart' ||
+      connection === 'timeout'
+     ) {
+      let reason = new Boom(lastDisconnect?.error)?.output.statusCode;
 
-    console.log(`Connection ${connection}, reconnecting...`);
+      console.log(`Connection ${connection}, reconnecting...`);
 
-    if (reason === DisconnectReason.loggedOut) {
-    console.log('Device Logged Out, Please Delete Session and Scan Again.');
-    process.exit();
-     }
+      if (reason === DisconnectReason.loggedOut) {
+      console.log('Device Logged Out, Please Delete Session and Scan Again.');
+      process.exit();
+      }
 
-     await startAztec();
+      await startAztec();
     } else if (connection === 'close') {
       console.log(`[ 🐲AZTEC ] Connection closed, reconnecting...`);
       await startAztec();
@@ -75,23 +74,28 @@ const { getAuthFromDatabase } = new Auth(sessionId);
   });
 
   vorterx.ev.on('messages.upsert', async (messages) => await MessageHandler(messages, vorterx));
-
   vorterx.ev.on('contacts.update', async (update) => await contact.saveContacts(update, vorterx));
+  
   const app = express();
-app.listen(PORT, () => {
-console.log(`Server is running on port ${PORT}/`);});
-
-await vorterx.connect();}
-
-async function readCommands(vorterx) {
-const commandFiles = fs.readdirSync('./Commands').filter((file) => file.endsWith('.js'));
-
-for (const file of commandFiles) {
-const command = require(`./Commands/${file}`);
-vorterx.cmd.set(command.name, command);}}
-
-startAztec();
-app.get("/qr", async (req, res) => {
+  app.get("/qr", async (req, res) => {
   res.setHeader("content-type", "image/png");
   res.send(await qrcode.toBuffer(qr_gen));
-});
+  });
+
+  app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}/`);
+  });
+
+  await vorterx.connect();
+ }
+
+async function readCommands(vorterx) {
+    const commandFiles = fs.readdirSync('./Commands').filter((file) => file.endsWith('.js'));
+
+    for (const file of commandFiles) {
+    const command = require(`./Commands/${file}`);
+    vorterx.cmd.set(command.name, command);
+   }
+   }
+
+startAztec();
