273c5db9b7c786a64eefcf23a3569650f01e7348
D3centX <109889758+DiegosonTech@users.noreply.github.com>
2023-10-22 16:05:11 +0200

    vorterx

--- mangoes/contact.js
+++ mangoes/contact.js
@@ -1,21 +1,17 @@
-      const getContact = async (jid, vorterx) => {
-      
-      const contact = await vorterx.contactDB.get(jid);
-      const username = contact?.name ?? 'User';
-      return { username, jid };
-      };
-  
-      const saveContacts = async (contacts, vorterx) => {
-      await Promise.all(
-      contacts.map(async (contact) => {
-      if (contact.id) {
-      await vorterx.contactDB.set(contact.id, contact.notify ?? '');
-        }
-        })
-        );
-        };
+const getContact = async (jid, vorterx) => {
+    const contact = await vorterx.contactDB.get(jid);
+    const username = contact?.name ?? 'User';
+    return { username, jid };
+    };
+const saveContacts = async (contacts, vorterx) => {
+    await Promise.all(
+    contacts.map(async (contact) => {
+    if (contact.id) {
+    await vorterx.contactDB.set(contact.id, contact.notify ?? '');
+  }}));
+  };
   
   module.exports = {
-    saveContacts,
-    getContact,
-    };
+  saveContacts,
+  getContact,
+  };
