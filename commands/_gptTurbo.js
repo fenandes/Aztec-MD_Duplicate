6b2651d0fa230df184fa27822c15646c2efa6259
diegoson <110030270+D3centX@users.noreply.github.com>
2023-10-30 08:36:01 +0200

    vorterx

--- commands/_gptTurbo.js
+++ commands/_gptTurbo.js
@@ -1,23 +1,25 @@
 //BY VORTERX
 //@DiegosonTech
-const fetch = require('node-fetch');
 
 module.exports = {
   name: "gpt",
   alias: ["ai", "openai", "chatgpt"],
   category: "CHATGPT",
   description: "Randomly search",
-  async xstart(vorterx,m,{ text, xReact, args }) {
-    
-    if (!text) {  
-    await xReact('❌'); return m.reply(`*Provide me a query ex who made Aztec*`);
+  async xstart(vorterx, m, { text, xReact, args }) {
+    if (!text) {
+      await xReact('❌');
+      return m.reply(`*Provide me a query ex who made Aztec*`);
     }
 
     try {
-      const response = await fetch(`https://xzn.wtf/api/openai?text=${encodeURIComponent(text)}&apikey=aztec`);
+      const fetch = await import('node-fetch');
+      const response = await fetch.default(
+        `https://xzn.wtf/api/openai?text=${encodeURIComponent(text)}&apikey=aztec`
+      );
       const result = await response.json();
       const aiTurbo = result.result;
-      
+
       const externalAdReply = {
         title: 'GPT TURBO 3.5K',
         mediaType: 1,
@@ -25,12 +27,15 @@ module.exports = {
         sourceUrl: '',
         showAdAttribution: true,
         thumbnail: 'https://i.ibb.co/9bfjPyH/1-t-Y7-MK1-O-S4eq-YJ0-Ub4irg.png',
-        renderLarger: true
-       };
+        renderLarger: true,
+      };
 
-       await m.reply(`*👤USER*: ${text}\n\n*🌳AZTEC GPT RESULTS ARE*: ${aiTurbo}`, { contextInfo: { externalAdReply } });
-      } catch (error) {console.error(error);
+      await m.reply(`*👤USER*: ${text}\n\n*🌳AZTEC GPT RESULTS ARE*: ${aiTurbo}`, {
+        contextInfo: { externalAdReply },
+      });
+    } catch (error) {
+      console.error(error);
       await m.reply('An error occurred while processing the request.');
-     }
     }
-  };
+  },
+};
