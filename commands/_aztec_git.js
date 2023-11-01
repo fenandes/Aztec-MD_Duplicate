a1717be968c07b685dbe631ef7e1ed8a6b1220c8
D3centX <109889758+DiegosonTech@users.noreply.github.com>
2023-11-01 21:55:39 +0200

    vorterxi

--- commands/_aztec_git.js
+++ commands/_aztec_git.js
@@ -1,26 +1,29 @@
 const axios = require("axios");
 const moment = require("moment-timezone");
 const { fancyText } = require("@viper-x/fancytext");
+const fs = require("fs");
+const { aztec_images } = require("../mangoes/encryptFunc.js");
 
 module.exports = {
   name: "sc",
   alias: ["script"],
   description: "Retrieve Aztec information",
   category: "General",
-  async xstart(vorterx, m, { xReact, text,args }) {
-     
-      const fs = require("fs");
-      const { aztec_images } = require("../mangoes/encryptFunc.js");
-      const aztecImage = fs.readFileSync("../lib/imogs.jpg");
-    
-      await xReact("🙋‍♂️");
-      let [date, time] = new Date()
-      .toLocaleString("en-IN", { timeZone: "Africa/Johannesburg" })
-      .split(",");
-      const { data: repoData } = await axios.get("https://api.github.com/repos/Vorterx/Aztec-MD");
-      const { data: commitData } = await axios.get("https://api.github.com/repos/Vorterx/Aztec-MD/commits");
+  async xstart(vorterx, m, { xReact, text, args }) {
+    const aztecImage = fs.readFileSync("./lib/imogs.jpg");
 
-      const gitMessage = fancyText(`
+    await xReact("🙋‍♂️");
+    let [date, time] = new Date()
+    .toLocaleString("en-IN", { timeZone: "Africa/Johannesburg" })
+    .split(",");
+    const { data: repoData } = await axios.get(
+      "https://api.github.com/repos/Vorterx/Aztec-MD"
+    );
+    const { data: commitData } = await axios.get(
+      "https://api.github.com/repos/Vorterx/Aztec-MD/commits"
+    );
+
+    const gitMessage = fancyText(`
       ╭──────────────────────╮
       │ *乂 AZTEC M D- REPO 乂*│
       ├──────────────────────┤
@@ -38,22 +41,22 @@ module.exports = {
     `);
 
     const aztecMessage = {
-       image: {url: await aztec_images()},
-       caption: gitMessage,
-       footer: "aztec",
-       headerType: 1,
-       contextInfo: {
-          externalAdReply: {
-          title: "Powerd by Aztec",
-          body: "Unlish your imagination",
+      image: { url: await aztec_images() },
+      caption: gitMessage,
+      footer: "aztec",
+      headerType: 1,
+      contextInfo: {
+        externalAdReply: {
+          title: "Powered by Aztec",
+          body: "Unleash your imagination",
           mediaType: 1,
           thumbnail: aztecImage,
           mediaUrl: "",
           sourceUrl: "",
         },
        },
-      };
+     };
 
      await vorterx.sendMessage(m.from, aztecMessage, { quoted: m });
-    },
-   };
+  },
+  };
