405c39b6139dbf2b5b63e3d1e8282cf69539d802
D3centX <109889758+DiegosonTech@users.noreply.github.com>
2023-11-18 17:39:38 +0200

    vorterx::

--- commanded/_fullpp_gc.js
+++ commanded/_fullpp_gc.js
@@ -1,27 +1,66 @@
+const Jimp = require('jimp');
+const fs = require('fs');
+
 module.exports = {
   name: 'gcfullp',
   description: 'Set group full picture dp',
   category: 'Group',
-  async xstart(vorterx,m,{isAdmin,isBotAdmin,xReact,text,args}) {
-    
-     if (!isAdmin) { await xReact("❌");
-      return m.reply(`This command is only for group admins`);
-        }
-      if (!isBotAdmin) { await xReact("❌");
-      return m.reply(`l must be an admin for me to help`);
-        }
+  async xstart(vorterx, m, { isAdmin, isBotAdmin, xReact, text, quoted, args }) {
+   
+    if (!isAdmin) {
+      await xReact("❌");
+      return m.reply('This command is only for group admins');
+    }
 
-        if (!/image/.test(mime)) {
-          await xReact("❌");
-          return vorterx.sendMessage(m.from,{text: `Please reply to a Picture`,},{ quoted: m });
-         }
-        await xReact("📷");
+    if (!isBotAdmin) {
+      await xReact("❌");
+      return m.reply('I must be an admin for me to help');
+    }
 
-        let quotedimage = await vorterx.downloadAndSaveMediaMessage(quoted);
-        var { preview } = await generatePP(quotedimage);
-        await vorterx.query({tag: "iq",attrs: {to: m.from,type: "set",xmlns: "w:profile:picture",},
-        content: [{tag: "picture",attrs: { type: "image" },content: preview,},],});
-        fs.unlinkSync(quotedimage);
-        groupPp = await vorterx.profilePictureUrl(m.from, "image");
+    const quotedMessage = m.quoted;
 
-        vorterx.sendMessage(m.from,{image: { url: ppgc },caption: `Profile picture has been updated`},{ quoted: m });
+    if (!quotedMessage || !quotedMessage.mimetype.includes('image')) {
+      await xReact("❌");
+      return vorterx.sendMessage(m.from, { text: 'Please reply to a picture' }, { quoted: m });
+    }
+
+    await xReact('📷');
+
+    try {
+      const quotedImageBuffer = await vorterx.downloadMediaMessage(quotedMessage);
+      const { img, preview } = await generatePP(quotedImageBuffer);
+
+      await updateGroupProfilePicture(vorterx, m.from, preview);
+      fs.unlinkSync(quotedImageBuffer);
+
+      const groupPpUrl = await getGroupProfilePictureUrl(vorterx, m.from);
+      await sendProfilePictureUpdatedMessage(vorterx, m.from, groupPpUrl, m);
+    } catch (error) {
+      console.error('An error occurred:', error);
+      await xReact("❌");
+      return m.reply('An error occurred while setting the group profile picture');
+    }
+   },
+ };
+
+ async function generatePP(buffer) {
+  const jimp = await Jimp.read(buffer);
+  const min = jimp.getWidth();
+  const max = jimp.getHeight();
+  const cropped = jimp.crop(0, 0, min, max);
+  return { img: await cropped.scaleToFit(720, 720).getBufferAsync(Jimp.MIME_JPEG),
+    preview: await cropped.normalize().getBufferAsync(Jimp.MIME_JPEG),
+  };
+}
+
+async function updateGroupProfilePicture(vorterx, groupId, previewImageBuffer) {
+  return vorterx.updateProfilePicture(groupId, previewImageBuffer);
+}
+
+async function getGroupProfilePictureUrl(vorterx, groupId) {
+  return vorterx.getProfilePicture(groupId, { thumbnail: false });
+}
+
+async function sendProfilePictureUpdatedMessage(vorterx, groupId, groupPpUrl, originalMessage) {
+  await vorterx.sendMessage(groupId, { image: { url: groupPpUrl }, caption: 'Profile picture has been updated' }, { quoted: originalMessage });
+}
