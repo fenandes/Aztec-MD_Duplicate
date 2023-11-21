5fa7f8c5e0896db179f9a944162b687185b0806d
D3centX <109889758+DiegosonTech@users.noreply.github.com>
2023-11-21 06:33:43 +0200

    vorterx:

---
+++ commanded/_titok_vid.js
@@ -0,0 +1,12 @@
+const downloadTikTokVideo = require("./path/to/downloadTikTokVideo.js");
+
+module.exports = {
+  name: 'tik',
+  alias: ['tiktok'],
+  description: 'TikTok Video Downloader',
+  category: 'Downloads',
+  async xstart(vorterx, m, { xReact, text }) {
+    await xReact("📤");
+    await downloadTikTokVideo(vorterx, m, { xReact, text });
+  },
+};
