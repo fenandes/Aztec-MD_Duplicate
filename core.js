7f159730423a1b2f7b20ed347f32d6e1be3087f3
D3centX <109889758+DiegosonTech@users.noreply.github.com>
2023-11-23 09:56:18 +0200

    vorterx

--- core.js
+++ core.js
@@ -1,4 +1,4 @@
-const { default: VorterxConnection, DisconnectReason, Browsers, delay, fetchLatestBaileysVersion, makeInMemoryStore, useMultiFileAuthState } = require('@adiwajishing/baileys');
+const { default: VorterxConnection, DisconnectReason, Browsers, delay, fetchLatestBaileysVersion, makeInMemoryStore, useMultiFileAuthState } = require('@whiskeysockets/baileys');
 const { Boom } = require('@hapi/boom');
 const P = require('pino');
 const express = require('express');
