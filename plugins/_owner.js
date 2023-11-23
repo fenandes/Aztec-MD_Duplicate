5b89eb4a4d78c0c3fdddd7f924ccabe7eb5d8045
D3centX <109889758+DiegosonTech@users.noreply.github.com>
2023-11-23 22:22:18 +0200

    vorterx

--- plugins/_owner.js
+++ plugins/_owner.js
@@ -8,27 +8,31 @@ module.exports = {
     await xReact("✔️");
 
     const ownerName = config.mods;
+    const userName = config.OWNER_NAME;
     const logo = "https://i.ibb.co/v47d4BL/IMG-20230429-WA0021.jpg";
     const sourceUrl = `https://wa.me/+${ownerName}?text=Hii+bro,I+am+a+big+fan+`;
 
     const buttonMessage = {
-         contacts: {
-         displayName: ownerName,
-         contacts: [{ vcard: `BEGIN:VCARD\nVERSION:3.0\nFN:${ownerName}\nORG:;\nTEL;type=CELL;type=VOICE;waid=${ownerName}:+${ownerName}\nEND:VCARD` }],
+      contacts: {
+        displayName: ownerName,
+        contacts: [{ vcard: `BEGIN:VCARD\nVERSION:3.0\nFN:${ownerName}\nORG:;\nTEL;type=CELL;type=VOICE;waid=${ownerName}:+${ownerName}\nEND:VCARD` }],
       },
       contextInfo: {
-          externalAdReply: {
+        stanzaId: m.key.id,
+        participant: m.key.remoteJID,
+        quotedMessage: m.message,
+        externalAdReply: {
           title: "Aztec MD.",
-          body: "Powerd by Aztec",
+          body: "Powered by Aztec",
           renderLargerThumbnail: true,
           thumbnail: logo,
           mediaType: 1,
           mediaUrl: "",
           sourceUrl: sourceUrl,
-         },
         },
-       };
+      },
+    };
 
     await vorterx.sendMessage(m.from, buttonMessage, { quoted: m });
-    },
-   };
+  },
+};
