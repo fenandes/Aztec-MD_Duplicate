525e3853d2a9d935766a0b0b96f04fc3b302a44e
D3centX <109889758+DiegosonTech@users.noreply.github.com>
2023-11-23 20:41:25 +0200

    vorterx

---
+++ plugins/_cystem_u.js
@@ -0,0 +1,45 @@
+const { formatp, runtime } = require("../mangoes/myFunc.js");
+const chalk = require("chalk");
+const { bubble } = require("@viper-x/fancytext");
+const { getLatestGPTVersion } = require("../lib/myModule.js");
+const os = require("os");
+const now = require("performance-now");
+
+module.exports = {
+  name: "system",
+  description: "To check the system status",
+  category: "user",
+  async xstart(vorterx, m, { xReact,text }) {
+    const latensi = now() - now();
+    await xReact("📟");
+
+    let aztec = `*乂 SYSTEM - STATUS*\n\n`;
+    aztec += `❲❒❳ *BotName :* ${process.env.BOTNAME}\n`;
+    aztec += `❲❒❳ *Version :* 3.0.0\n`;
+    aztec += `❲❒❳ *RAM :* _${formatp(os.totalmem() - os.freemem())}/${formatp(
+      os.totalmem()
+    )}_\n`;
+    aztec += `❲❒❳ *Speed : _${latensi.toFixed(4)}sec_*\n`;
+    aztec += `❲❒❳ *Runtime :* _${runtime(process.uptime())}_\n`;
+    aztec += `❲❒❳ *Platform :* ${os.platform()}.com\n`;
+    aztec += `❲❒❳ *Platform ID :* ${os.hostname()}\n\n`;
+    aztec += `❲❒❳ *Latest GPT Version :* ${await getLatestGPTVersion()}\n\n`;
+    aztec += `*©vorterx-team*`;
+
+    const formattedAztec = chalk.bold(aztec);
+
+    const D3centX = [
+      { name: "selectAztec 1", text: formattedAztec },
+      { name: "selectAztec 2", text: formattedAztec },
+      { name: "selectAztec 3", text: formattedAztec },
+      { name: "selectAztec 4", text: bubble(aztec, "blue") },
+      { name: "selectAztec 5", text: bubble(aztec, "green") },
+      { name: "selectAztec 6", text: bubble(aztec, "purple") },
+    ];
+
+    const selectedAztec = D3centX[Math.floor(Math.random() * D3centX.length)];
+
+    const img = `https://i.ibb.co/GnZ0J9K/IMG-20230723-WA0085.jpg`;
+    vorterx.sendMessage(m.from, { image: { url: img }, caption: selectedAztec.text });
+  },
+};
