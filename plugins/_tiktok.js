147b34b4fc3f2d38e4482d860098ef2cd42b5a80
D3centX <109889758+DiegosonTech@users.noreply.github.com>
2023-11-25 07:57:12 +0200

    vorterx

--- plugins/_tiktok.js
+++ plugins/_tiktok.js
@@ -1,41 +1,42 @@
-const fetch = require('node-fetch');
+const axios = require('axios');
 
 module.exports = {
   name: 'tik',
   alias: ['tiktok'],
   description: 'To download tiktok videos',
   category: 'Downloads',
-  async xstart(vorterx,m,{text,args,xReact}) {
-if (!text) {
-    await xReact('❌');
-    return m.reply('Please provide a valid TikTok video URL.');
-  }
-
-  const apiKey = '29y8XIYL';
-  let caption = '';
-
-  try {
-    const downloadUrl = `https://api.botcahx.live/api/dowloader/tiktok?url=${encodeURIComponent(
-      text
-    )}&apikey=${apiKey}`;
-
-    await xReact('📤');
-    const response = await fetch(downloadUrl);
-    const data = await response.json();
-
-    if (data.success) {
-      const videoTitle = data.result.title;
-      const videoViews = data.result.views;
-      const videoLikes = data.result.likes;
-      const videoPublished = data.result.published;
-
-      caption = `🌳TITLE: ${videoTitle}\n👀VIEWS: ${videoViews}\n👍LIKES: ${videoLikes}\n🙌PUBLISHED: ${videoPublished}`;
-
-      vorterx.sendMessage(m.from, `${caption}`);
-    } else {
+  async xstart(vorterx, m, { text, args, xReact }) {
+    
+    if (!text) {
+      await xReact('❌');
+      return m.reply('Please provide a valid TikTok video URL.');
     }
-  } catch (error) {
-    console.error('An error occurred:', error);
-    m.reply('An error occurred while processing the TikTok video');
+
+    const apiKey = '29y8XIYL';
+    let caption = '';
+
+    try {
+      const downloadUrl = `https://api.botcahx.live/api/dowloader/tiktok?url=${encodeURIComponent(
+        text
+      )}&apikey=${apiKey}`;
+
+      await xReact('📤');
+      const response = await axios.get(downloadUrl);
+      const data = response.data;
+
+      if (data.success) {
+        const videoTitle = data.result.title;
+        const videoViews = data.result.views;
+        const videoLikes = data.result.likes;
+        const videoPublished = data.result.published;
+
+        caption = `🌳TITLE: ${videoTitle}\n👀VIEWS: ${videoViews}\n👍LIKES: ${videoLikes}\n🙌PUBLISHED: ${videoPublished}`;
+
+        vorterx.sendMessage(m.from, `${caption}`);
+      } else {}
+    } catch (error) {
+      console.error('An error occurred:', error);
+      m.reply('An error occurred while processing the TikTok video');
+    }
   }
-}}
+};
