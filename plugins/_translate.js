202ceebed53330365a8908f003e43a3d0ee7a06b
D3centX <109889758+DiegosonTech@users.noreply.github.com>
2023-11-25 09:00:26 +0200

    vorterx

---
+++ plugins/_translate.js
@@ -0,0 +1,38 @@
+const translate = require('translate-google-api');
+
+module.exports = {
+  name: 'tr',
+  alias: ['translate'],
+  description: 'Translate any usable language',
+  category: 'Extra',
+  async xstart(vorterx, m, { text, args, xReact }) {
+    if (!text) {
+      await xReact('👀');
+      return m.reply('Example: tr Damn l love this Diegoson eish');
+    }
+
+    let lang;
+    let data;
+
+    if (text && m.quoted && m.quoted.text) {
+      lang = text.slice(0, 2);
+      data = m.quoted.text;
+    } else if (text) {
+      lang = text.slice(0, 2);
+      data = text.substring(2).trim();
+    }
+   
+    try {
+      await xReact('📝');
+      const result = await translate(data, { to: lang });
+      const caption = `📝Text: ${data}\n🧘Translated: ${result}`;
+      await m.reply(caption);
+    } catch (error) {
+      let errorMessage = 'An error occurred while translating';
+      if (error.code === 'NOT_SUPPORTED') {
+        errorMessage = 'The language is not supported';
+      }
+      await m.reply(errorMessage);
+    }
+  },
+};
