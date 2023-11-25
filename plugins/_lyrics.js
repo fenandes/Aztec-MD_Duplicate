55d26c5d84f1c6694b2aa5134590a9684d85167e
D3centX <109889758+DiegosonTech@users.noreply.github.com>
2023-11-25 10:33:51 +0200

    vorterx

---
+++ plugins/_lyrics.js
@@ -0,0 +1,35 @@
+const axios = require('axios');
+
+module.exports = {
+  name: 'lyrics',
+  description: 'To get the lyrics for your music',
+  category: 'Search',
+  async xstart(vorterx, m, { text, args, xReact }) {
+    if (!text) {
+      await xReact('❌');
+      return m.reply('Please provide a song name');
+    }
+
+    const apiUrl = `https://api.neoxr.eu/api/song?q=${encodeURIComponent(text)}`;
+
+    try {
+      await xReact('🍏');
+      const response = await axios.get(apiUrl);
+      const data = response.data;
+
+      if (response.status === 200) {
+        const lyrics = data.lyrics;
+        const thumbnail = data.thumbnail;
+
+        const sendLyrics = `Lyrics for "${text}":\n\n${lyrics}`;
+        vorterx.sendMessage(m.from, { image: { url: thumbnail, caption: sendLyrics } });
+      } else {
+        const errorMessage = data.message || 'Unknown error occurred';
+        vorterx.sendMessage(m.from, `Failed to get lyrics: ${errorMessage}`, 'text');
+      }
+    } catch (error) {
+      console.error(error);
+      m.reply('An error occurred while fetching lyrics', 'text');
+    }
+  },
+};
