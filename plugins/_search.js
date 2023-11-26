0ef7f5dd81eda273a3e609d4d281bd5c2d872a07
D3centX <109889758+DiegosonTech@users.noreply.github.com>
2023-11-26 16:11:14 +0200

    vorterx

--- plugins/_search.js
+++ plugins/_search.js
@@ -1,19 +1,20 @@
+//
 const axios = require('axios');
 const google = require('google-it');
 const chalk = require('chalk');
 
 module.exports = {
-  name: 'github',
-  alias: ['gt'],
-  async xstart(vorterx, m, { args, text, xReact, prefix }) {
+  name: 'search',
+  async xstart(vorterx, m, { args, text, xReact }) {
     if (args[0] === 'search') {
       const Commands = [
         'github',
-        'google'
+        'google',
+        'weather'
       ];
 
-      const prefix = process.env.PREFIX;
-      const commandS = Commands.map(command => `*${prefix}${command}*`).join(', ');
+      const envPrefix = process.env.PREFIX;
+      const commandS = Commands.map(command => `*${envPrefix}${command}*`).join(', ');
 
       const helpMsg = `Available commands for search:\n\n${commandS}`;
 
@@ -48,7 +49,7 @@ module.exports = {
           const repoNames = userRepos.data.map(repo => repo.name);
           const repoList = repoNames.join('\n');
 
-          vorterx.sendMessage(m.from, {image: { url: avatar_url, mimetype: 'image/jpeg' },caption: rezText8 + '\n\n*📚 Repositories:*\n' + repoList,}, { quoted: m });
+          vorterx.sendMessage(m.from, { image: { url: avatar_url, mimetype: 'image/jpeg' }, caption: rezText8 + '\n\n*📚 Repositories:*\n' + repoList }, { quoted: m });
 
           break;
 
@@ -60,9 +61,8 @@ module.exports = {
       }
 
       case 'google': {
-        
         await xReact("🔍");
-        google({ 'query': text }).then(res => {
+        google({ query: text }).then(res => {
           let aztec = `🔎 *GOOGLE SEARCH RESULTS* 🔍\n\n${text}\n\n`;
 
           for (let g of res) {
@@ -80,29 +80,33 @@ module.exports = {
         });
 
         break;
-
-        case 'wearther': {
-        
-       if (!text) {
-        await xReact('❌');
-        return m.reply('*Please provide the city of the the country e.g weather Johannesburg*');
       }
 
-     try {
-      const apiKey = 'e409825a497a0c894d2dd975542234b0';
-      const weatherData = await getWeatherData(text, apiKey);
+      case 'weather': {
+        if (!text) {
+          await xReact('❌');
+          return m.reply('*Please provide the city of the country e.g weather Johannesburg*');
+        }
 
-      const weatherReport = formatWeatherReport(weatherData);
+        try {
+          await xReact('🌈');
+          const apiKey = 'e409825a497a0c894d2dd975542234b0';
+          const weatherData = await getWeatherData(text, apiKey);
 
-      const gifUrl = "https://media.tenor.com/bC57J4v11UcAAAPo/weather-sunny.mp4";
+          const weatherReport = formatWeatherReport(weatherData);
 
-      await vorterx.sendMessage(m.from, { body: weatherReport, url: gifUrl, caption: weatherReport, gifPlayback: true }, m);
-     } catch (error) {
-      console.error(error);
-      m.reply("Failed to fetch weather information. Please try again later.");
+          const gifUrl = "https://media.tenor.com/bC57J4v11UcAAAPo/weather-sunny.mp4";
+
+          await vorterx.sendMessage(m.from, { body: weatherReport, url: gifUrl, caption: weatherReport, gifPlayback: true }, m);
+        } catch (error) {
+          console.error(error);
+          m.reply("Failed to fetch weather information. Please try again later.");
+        }
+        break;
+      }
     }
-  }
- };
+  },
+};
 
 async function getWeatherData(location, apiKey) {
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=${apiKey}&language=tr`;
@@ -124,9 +128,4 @@ function formatWeatherReport(weatherData) {
   const weatherReport = `🌤 *Weather Report* 🌤\n\n🔎 *Search Location:* ${name}\n*💮 Country:* ${country}\n🌈 *Weather:* ${weatherDescription}\n🌡️ *Temperature:* ${temp}°C\n❄️ *Minimum Temperature:* ${temp_min}°C\n📛 *Maximum Temperature:* ${temp_max}°C\n💦 *Humidity:* ${humidity}%\n🎐 *Wind:* ${speed} km/h\n`;
 
   return weatherReport;
-}
-    break;
-      }
-    }
-  },
-};
+  }
