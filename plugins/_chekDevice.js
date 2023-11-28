ae7457082a5349796c1c8d3f6e3b25a6c30787bb
D3centX <109889758+DiegosonTech@users.noreply.github.com>
2023-11-28 19:03:25 +0200

    vorterx ::

--- plugins/_chekDevice.js
+++ plugins/_chekDevice.js
@@ -8,7 +8,7 @@ module.exports = {
     try {
       await xReact('❌');
       if (!quoted && !m.mentionedJid) {
-      return m.reply('_Please reply to someone to get their device information._');
+        return m.reply('_Please reply to someone to get their device information._');
       }
 
       await xReact('📱');
@@ -19,7 +19,12 @@ module.exports = {
       const device = `DEVICE: ${deviceInfo}`;
       
       const replyMsg = `${userMention}\n${userNumber}\n${device}`;
-      m.reply(replyMsg);
+      const imageUrl = 'https://graph.org/file/1c594cbc8d4c25046b803.jpg';
+      
+      vorterx.sendMessage(m.from, {
+        image: { url: imageUrl },
+        caption: replyMsg
+      });
     } catch (error) {
       console.error(error);
       m.reply('_An error occurred while retrieving device information._');
