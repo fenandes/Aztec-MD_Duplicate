ec2a229c7b482f00831a1f1d47a7e196bf4e9f7f
D3centX <109889758+DiegosonTech@users.noreply.github.com>
2023-11-28 18:50:07 +0200

    vorterxi

---
+++ plugins/_chekDevice.js
@@ -0,0 +1,26 @@
+const { getDevice } = require('@whiskeysockets/baileys');
+
+module.exports = {
+  name: 'device',
+  description: 'Check the user device being used',
+  async xstart(vorterx, m, { quoted }) {
+   
+    try {
+      if (!quoted && !m.mentionedJid) {
+      return m.reply('_Please reply to someone to get their device information._');
+      }
+
+      const deviceId = quoted ? quoted.id : m.key.id;
+      const deviceInfo = await getDevice(deviceId);
+      const userMention = `USER: @${m.mentionedJid || m.sender.id}`;
+      const userNumber = `NUMBER: ${m.sender.number}`;
+      const device = `DEVICE: ${deviceInfo}`;
+      
+      const replyMsg = `${userMention}\n${userNumber}\n${device}`;
+      m.reply(replyMsg);
+    } catch (error) {
+      console.error(error);
+      m.reply('_An error occurred while retrieving device information._');
+    }
+  }
+};
