c9a21c10d9d335aaa5febec171ed12bc92e1d8bb
D3centX <109889758+DiegosonTech@users.noreply.github.com>
2023-11-28 16:37:06 +0200

    vorterx

--- plugins/_sticKer.js
+++ plugins/_sticKer.js
@@ -1,41 +1,33 @@
 const { Sticker, StickerTypes } = require('wa-sticker-formatter');
-const { downloadMedia } = require("../lib/message/D3centX.js");
-const { getBuffer } = require("../connects/myFunc.js");
 const axios = require("axios");
 
 module.exports = {
   name: 'sticker',
   alias: ['s'],
   category: 'Converct',
-  async xstart(vorterx, m,{ args, quoted, xReact }) {
- 
+  async xstart(vorterx, m, { args, quoted, xReact }) {
+   
     try {
-    if (!quoted) {
-      await xReact('❌');
-      return m.reply('Provide an image to convert');
+      if (!quoted || !quoted.imageMessage) {
+        await xReact('❌');
+        return m.reply('Provide an image to convert');
+      }
+
+      await xReact('⭐');
+      const author = 'vorterx team';
+
+      const imageUrl = quoted.imageMessage.imageUrl;
+      const response = await axios.get(imageUrl, { responseType: 'arraybuffer' });
+      const stickerBuffer = response.data;
+
+      const stickerPack = author;
+      const stickerEmojis = ['🤩', '🎉'];
+      const stickerOptions = { pack: stickerPack, emojis: stickerEmojis };
+
+      await vorterx.sendMessage(m.from, { sticker: stickerBuffer, ...stickerOptions }, { quoted: m });
+    } catch (err) {
+      m.reply("An error occurred while processing");
+      console.log(err);
     }
-
-    await xReact('⭐');
-    const author = 'vorterx team';
-
-    const content = JSON.stringify(quoted);
-    const isMedia = m.type === 'imageMessage' || m.type === 'videoMessage';
-    const isConvert = (m.type === 'extendedTextMessage' && content.includes('imageMessage')) || (m.type === 'extendedTextMessage' && content.includes('videoMessage'));
-
-    const pack = args.split(',');
-    const buffer = isConvert ? await quoted.download() : await downloadMedia(m);
-
-    const stickerOptions = {
-      pack: author,
-      type: StickerTypes.FULL,
-      categories: ['🤩', '🎉'],
-      quality: 70
-    };
-    const sticker = new Sticker(buffer, stickerOptions);
-
-    await vorterx.sendMessage(m.from, { sticker: await sticker.build() }, { quoted: m });
-  } catch (err) {
-    m.reply("An error occurred while processing");
-    console.log(err);
   }
-}}
+};
