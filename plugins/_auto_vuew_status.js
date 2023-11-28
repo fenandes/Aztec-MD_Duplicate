1a2772b994cc6ae7a4004a18e9700898288c8489
D3centX <109889758+DiegosonTech@users.noreply.github.com>
2023-11-28 06:38:17 +0200

    vorterx::

--- plugins/_auto_vuew_status.js
+++ plugins/_auto_vuew_status.js
@@ -1,13 +1,16 @@
 const config = require('../config.js');
+const vorterx = require('../lib/message/vorterx.js');
 
 vorterx.ev.on('messages.upsert', async chatUpdate => {
-            const botNumber = await vorterx.decodeJid(vorterx.user.id);
-            const view = chatUpdate.messages[0]
-                 if (!view.message) return
-                 view.message = (Object.keys(view.message)[0] === 'ephemeralMessage') ? view.m.ephemeralMessage.message : view.message
-                 if(mek.key && view.key.remoteJid === 'status@broadcast'){
-                    last_status = view;
-                    if( config.read_status === 'true'){ await vorterx.readMessages([view.key]);}
-                     
-                    return;
-                    }
+    const botNumber = await vorterx.decodeJid(vorterx.user.id);
+    const view = chatUpdate.messages[0];
+    if (!view.message) return;
+    view.message = (Object.keys(view.message)[0] === 'ephemeralMessage') ? view.m.ephemeralMessage.message : view.message;
+    if (view.key && view.key.remoteJid === 'status@broadcast') {
+        last_status = view;
+        if (config.read_status === 'true') {
+            await vorterx.readMessages([view.key]);
+        }
+        return;
+    }
+});
