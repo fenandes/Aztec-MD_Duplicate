5480f36a28ea10008c9853ed52093aa28e39a7ae
D3centX <109889758+DiegosonTech@users.noreply.github.com>
2023-11-28 13:57:41 +0200

    vorterx

---
+++ plugins/_imgur.js
@@ -0,0 +1,29 @@
+const { generateRandomString } = require('./helperFunctions');
+
+module.exports = {
+  name: 'url',
+  category: 'Convert',
+  description: 'Convert image to Imgur-like link',
+  async start(vorterx, m, { text, quoted, mime, args }) {
+    
+    if (text.toLowerCase() !== 'url') {
+      await xReact('❌');
+      return m.reply('_Please reply to an image with the command "url"._');
+    }
+
+    if (mime.startsWith('image/')) {
+      const imageUrl = quoted ? quoted.url : m.url;
+      const isImage = imageUrl.match(/\.(jpeg|jpg|gif|png)$/);
+
+      if (isImage) {
+        await xReact('✔️');
+        const imgurLink = `https://i.imgur.com/${generateRandomString(7)}.jpg`;
+        vorterx.sendMessage(m.from, { url: imgurLink });
+      } else {
+        m.reply('_The provided URL is not a valid image._');
+      }
+    } else {
+      m.reply('_Please provide an image._');
+    }
+  },
+};
