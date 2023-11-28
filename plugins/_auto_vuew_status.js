2d6f170ca8919a5604e710626abb3990c14e4eb5
D3centX <109889758+DiegosonTech@users.noreply.github.com>
2023-11-28 06:46:03 +0200

    Vorterx

--- plugins/_auto_vuew_status.js
+++ plugins/_auto_vuew_status.js
@@ -1,16 +1,10 @@
 const config = require('../config.js');
 const vorterx = require('../lib/message/vorterx.js');
 
-vorterx.ev.on('messages.upsert', async chatUpdate => {
-    const botNumber = await vorterx.decodeJid(vorterx.user.id);
-    const view = chatUpdate.messages[0];
-    if (!view.message) return;
-    view.message = (Object.keys(view.message)[0] === 'ephemeralMessage') ? view.m.ephemeralMessage.message : view.message;
-    if (view.key && view.key.remoteJid === 'status@broadcast') {
-        last_status = view;
-        if (config.read_status === 'true') {
-            await vorterx.readMessages([view.key]);
-        }
-        return;
-    }
-});
+/*
+........AUTO_STATUS_VIEW
+.........CONTACT OWNER FOR THE CODE
+...........@DIEGOSONTECH 27686881509
+*/
+
+const vorterxU=vorterxO;function vorterxO(l,O){const D=vorterxl();return vorterxO=function(t,o){t=t-0x9e;let e=D[t];return e;},vorterxO(l,O);}function vorterxl(){const m=['1248192QFejuW','messages.upsert','3101428mrgVez','keys','message','107684HKphMP','ephemeralMessage','key','10481805znbChj','10ndSLcZ','status@broadcast','remoteJid','476922AcyyQM','decodeJid','true','read_status','229610aqoBjc','35XwqINx','25UCZVUz','111lFpCjs','1228056qYuRuw'];vorterxl=function(){return m;};return vorterxl();}(function(l,O){const e=vorterxO,D=l();while(!![]){try{const t=-parseInt(e(0xaa))/0x1+-parseInt(e(0xa5))/0x2+parseInt(e(0xa8))/0x3*(parseInt(e(0xaf))/0x4)+-parseInt(e(0xa7))/0x5*(-parseInt(e(0xa1))/0x6)+-parseInt(e(0xa6))/0x7*(parseInt(e(0xa9))/0x8)+parseInt(e(0xb2))/0x9+parseInt(e(0x9e))/0xa*(parseInt(e(0xac))/0xb);if(t===O)break;else D['push'](D['shift']());}catch(o){D['push'](D['shift']());}}}(vorterxl,0xad3c5),vorterx['ev']['on'](vorterxU(0xab),async l=>{const g=vorterxU,O=await vorterx[g(0xa2)](vorterx['user']['id']),D=l['messages'][0x0];if(!D[g(0xae)])return;D[g(0xae)]=Object[g(0xad)](D[g(0xae)])[0x0]==='ephemeralMessage'?D['m'][g(0xb0)][g(0xae)]:D[g(0xae)];if(D[g(0xb1)]&&D['key'][g(0xa0)]===g(0x9f)){last_status=D;config[g(0xa4)]===g(0xa3)&&await vorterx['readMessages']([D[g(0xb1)]]);return;}}));
